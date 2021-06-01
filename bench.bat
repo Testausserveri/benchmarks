52f8e1983f9bae32924364ecc0cb9e3aa128bd6f
Garrett Webb <webbgamers@gmail.com>
2021-05-31 23:50:26 -0400

    feat: Add Windows support and compile on demand (#2)
    * add windows support
    
    * compile programs during testing
    
    * Update README.md
    
    Remove build instructions since the scripts now handle that, add links for easier installation of dependencies, and add running instructions.

---
+++ bench.bat
@@ -0,0 +1,37 @@
+@echo off
+echo Very cool language benchmark script
+echo ---
+echo Node.js
+node perf.js
+echo ---
+echo Lua (LuaJIT)
+luajit perf.lua
+echo ---
+echo Rust
+cd perfrustsrc
+cargo run -q --release
+cd ..
+echo ---
+echo Go
+go run perf.go
+echo ---
+echo Python (PyPy)
+pypy3 perf.py
+echo ---
+echo C (GCC)
+gcc perf.c -Ofast -o perfc.exe
+perfc.exe
+echo ---
+echo C++ (G++)
+g++ perf.cpp -Ofast -o perfcpp.exe
+perfcpp.exe
+echo ---
+echo C#
+cd perfcsharpsrc
+dotnet run --configuration Release --verbosity quiet
+cd ..
+echo ---
+echo Java
+javac perf.java
+java perf
+echo ---
